05d8649632e575fbed4cb3206d387bf84ca8345c
Roman Sopotov <fr0st1x@outlook.com>
2023-02-11 16:30:13 +0300

    ex05_user

---
+++ ex05_user/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Как твоё имя? Назовись: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оля")
+{
+    Console.Write("Ты пупсик, ");
+    Console.Write(username);
+    Console.Write("!");
+}
+else
+{
+    Console.Write("Что это за имя такое дурацкое? ");
+    Console.Write(username);
+    Console.Write("...");
+}
