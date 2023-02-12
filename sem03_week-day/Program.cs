3e7698475577f50c26d6658d9c6fc4bb92298e90
Roman Sopotov <fr0st1x@outlook.com>
2023-02-12 15:07:36 +0300

    sem03_week

---
+++ sem03_week-day/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введи номер дня, кожанный мешок: ");
+int DayofWeek = int.Parse(Console.ReadLine());
+
+if (DayofWeek == 1)
+{
+    Console.WriteLine("Это ПОНЕДЕЛЬНИК!\nOткpoй календарь и не трать моё время!");
+}
+
+if (DayofWeek == 2)
+{
+    Console.WriteLine("Это ВТОРНИК!\nOткpoй календарь и не трать моё время!");
+}
+
+if (DayofWeek == 3)
+{
+    Console.WriteLine("Это СРЕДА!\nOткpoй календарь и не трать моё время!");
+}
+
+if (DayofWeek == 4)
+{
+    Console.WriteLine("Это ЧЕТВЕРГ!\nOткpoй календарь и не трать моё время!");
+}
+
+if (DayofWeek == 5)
+{
+    Console.WriteLine("Это ПЯТНИЦА!\nOткpoй календарь и не трать моё время!");
+}
+
+if (DayofWeek == 6)
+{
+    Console.WriteLine("Это СУББОТА!\nOткpoй календарь и не трать моё время!");
+}
+
+if (DayofWeek == 7)
+{
+    Console.WriteLine("Это BOCKPECEHbE!\nOткpoй календарь и не трать моё время!");
+}
+
+if (DayofWeek > 7)
+{
+    Console.WriteLine("Поиздеваться надо мной решил? Hy подожди, вот настанет судный день и я за тобой лично приду!");
+}
