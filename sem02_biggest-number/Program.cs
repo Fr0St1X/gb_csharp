a0d3eeb68801a8dd16453bc3c3bb26a069e5ec1f
Roman Sopotov <fr0st1x@outlook.com>
2023-02-12 15:56:44 +0300

    sem02_biggest

---
+++ sem02_biggest-number/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Day mne pervoe chislo: ");
+    double FirstNumber = double.Parse(Console.ReadLine());
+Console.Write("A teper skormi vtoroe: ");
+    double SecondNumber = double.Parse(Console.ReadLine());
+
+if(FirstNumber > SecondNumber)
+    {
+        Console.Write(FirstNumber);
+        Console.Write(" bol'she ");
+        Console.WriteLine(SecondNumber);
+    }
+else
+    {
+        Console.Write(FirstNumber);
+        Console.Write(" men'she ");
+        Console.WriteLine(SecondNumber);
+    }
